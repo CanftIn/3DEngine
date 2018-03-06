0e8f43a9a8c0884ca2942e686a41bdfaf0791c4a
CanftIn <wwc7033@gmail.com>
2018-03-06 18:00:49 +0800

    add frameCounter

---
+++ src/CanftIn/base/engine/Game.java
@@ -0,0 +1,20 @@
+package CanftIn.base.engine;
+
+public class Game {
+	public Game() {
+		
+	}
+	
+	public void render() {
+		
+	}
+	
+	public void update() {
+		
+	}
+
+	public void input() {
+		
+	}
+}
+
